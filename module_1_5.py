0e0af1c359643ac01d0b2bee12abe7b183e9b768
preva65 <89paul1965@gmail.com>
2024-10-31 22:42:17 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = ('треугольник', False, ['A', 'B', 'C'],  13)
+print('Immutable tuple:',(immutable_var))
+        # immutable_var.remove(13)
+        # print(immutable_var)
+
+mutable_list = ['треугольник', True, ['A', 'B', 'C'], 13]
+        #print(mutable_list)
+mutable_list.remove (13)
+mutable_list[2][0]='A+n'
+print('Mutable list:',(mutable_list))
